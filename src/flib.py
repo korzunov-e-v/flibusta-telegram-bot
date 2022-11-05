71155b8fc54522047e10239260ee4eded0251f2d
ekorzunov <korzunov.e.v@gmail.com>
2022-11-05 14:46:34 +0500

    initial commit

---
+++ src/flib.py
@@ -0,0 +1,175 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.parse
+import os
+import re
+
+all_formats = ['fb2', 'epub', 'mobi', 'pdf']
+site = 'http://flibusta.is'
+
+
+class Book:
+    def __init__(self, book_id):
+        self.id = book_id
+        self.title = ''
+        self.author = ''
+        self.link = ''
+        self.formats = {}
+        self.cover = ''
+
+    def __str__(self):
+        return '{} - {} ({})'.format(self.title, self.author, self.id)
+
+
+def get_page(url):
+    r = urllib.request.urlopen(url)
+    html_bytes = r.read()
+    html = html_bytes.decode("utf-8")
+    parser = "html.parser"
+    soup = BeautifulSoup(html, parser)
+    return soup
+
+
+def scrape_books(request_text):
+    query_text = urllib.parse.quote(request_text)
+    url_mask = "http://flibusta.is/booksearch?ask={request_str}&chb=on"
+    url = url_mask.format(request_str=query_text)
+
+    sp = get_page(url)
+
+    target_div = sp.find('div', attrs={'class': 'clear-block', 'id': 'main'})
+    target_ul_list = target_div.findChildren('ul', attrs={'class': ''})
+
+    if len(target_ul_list) == 0:
+        return None
+
+    target_ul=target_ul_list[0]
+    li_list = target_ul.find_all("li")
+
+    link_list = [a for a in (("http://flibusta.is" + li.a.get('href') + '/') for li in li_list)]
+    # author_list = [a for a in ((l.find_all('a')[1].text) for l in li_list)]
+    author_list = []
+    for li in li_list:
+        a_list = li.find_all('a')
+        if len(a_list) > 1:
+            author_list_l = a_list[1:]
+            author = ', '.join([a.text for a in author_list_l])
+        else:
+            author = '[автор не указан]'
+        author_list.append(author)
+
+    title_list = [a for a in ((li.find_all('a')[0].text) for li in li_list)]
+    book_id_list = [str(li.a.get('href')).replace('/b/', '') for li in li_list]
+
+    result = []
+    for i in range(len(book_id_list)):
+        book = Book(book_id_list[i])
+        book.title = title_list[i]
+        book.author = author_list[i]
+        book.link = link_list[i]
+        result.append(book)
+
+    return result
+
+
+def scrape_books_mbl(title, author):
+    title_q = urllib.parse.quote(title)
+    author_q = urllib.parse.quote(author)
+    url = f"http://flibusta.is/makebooklist?ab=ab1&t={title_q}&ln={author_q}&sort=sd2&"
+
+    sp = get_page(url)
+    target_form = sp.find('form', attrs={'name': 'bk'})
+
+    if target_form is None:
+        return None
+
+    div_list = target_form.find_all('div')
+
+    link_list, title_list, book_id_list, author_list = [], [], [], []
+    for d in div_list:
+        b_href = d.find('a', attrs={'href': re.compile('/b/')}).get('href')
+        link = "http://flibusta.is" + b_href + '/'
+        link_list.append(link)
+
+        title = d.find('a', attrs={'href': re.compile('/b/')}).text
+        title_list.append(title)
+
+        book_id = b_href.replace('/b/', '')
+        book_id_list.append(book_id)
+
+        a_list = d.find_all('a', attrs={'href': re.compile('/a/')})
+        if len(a_list) > 1:
+            author_list_l = a_list[1:]
+            author = ', '.join([a.text for a in author_list_l[::-1]])
+        else:
+            author = '[автор не указан]'
+        author_list.append(author)
+
+    result = []
+    for i in range(len(book_id_list)):
+        book = Book(book_id_list[i])
+        book.title = title_list[i]
+        book.author = author_list[i]
+        book.link = link_list[i]
+        result.append(book)
+
+    return result
+
+
+def get_book_by_id(book_id):
+    book = Book(book_id)
+    book.link = site + '/b/' + book_id + '/'
+
+    sp = get_page(book.link)
+    target_div = sp.find('div', attrs={'class': 'clear-block', 'id': 'main'})
+
+    target_h1 = target_div.find('h1', attrs={'class': 'title'})
+    book.title = target_h1.text
+
+    target_img = target_div.find('img', attrs={'alt': 'Cover image'})
+    if target_img:
+        book.cover = site + target_img.get('src')
+    else:
+        book.cover = None
+
+    format_li_list = target_div.find_all('a', string=re.compile('fb2|epub|mobi|pdf'))
+    for a in format_li_list:
+        b_format = a.text
+        link = a.get('href')
+        book.formats[b_format] = site + link
+
+    book.author = target_h1.findNext('a').text
+
+    return book
+
+
+def download_book_cover(book):
+    c_response = requests.get(book.cover)
+    c_full_path = os.path.join(os.getcwd(), "books", book.id, 'cover.jpg')
+    os.makedirs(os.path.dirname(c_full_path), exist_ok=True)
+    open(os.path.join(c_full_path), "wb").write(c_response.content)
+
+
+def download_book(book: Book, b_format):
+    book_id = book.id
+    book_url = book.formats[b_format]
+
+    try:
+        b_response = requests.get(book_url, timeout=10)
+    except requests.exceptions.Timeout as e: 
+        return None
+
+    if not b_response.ok:
+        return None
+
+    n_index = str(b_response.headers['content-disposition']).index('=')
+    b_filename = b_response.headers['content-disposition'][n_index + 1::].replace('\"', '')
+    if b_filename.endswith('.fb2.zip'):
+        b_filename = b_filename.removesuffix('.zip')
+
+    b_full_path = os.path.join(os.getcwd(), "books", book_id, b_filename)
+    os.makedirs(os.path.dirname(b_full_path), exist_ok=True)
+    open(os.path.join(b_full_path), "wb").write(b_response.content)
+
+    return b_full_path
---
+++ src/flibusta_bot.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Basic example for a bot that uses inline keyboards. For an in-depth
+explanation, check out
+https://github.com/python-telegram-bot/python-telegram-bot/wiki/InlineKeyboard-Example.
+"""
+import logging
+import flib
+import os
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler
+from telegram.ext import CallbackQueryHandler, CallbackContext
+
+from telegram.ext import MessageHandler, Filters
+from dotenv import dotenv_values
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    settings = dotenv_values(dotenv_path)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    encoding="utf-8",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger(__name__)
+
+logger_search = logging.getLogger("search_history")
+handler = logging.FileHandler(filename="search_log.log", encoding="utf-8")
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger_search.addHandler(handler)
+
+
+def start_callback(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Введите название книги (без автора) ИЛИ добавьте фамилию автора на "
+        "новой строке. \n\nПример:\n\n1984\nОруэлл"
+    )
+
+
+def find_the_book(update: Update, context: CallbackContext) -> None:
+    log_command = "find_the_book".ljust(20)
+    log_user_id = update.effective_user.id
+    log_user_name = update.effective_user.name
+    log_user_full_name = update.effective_user.full_name
+    log_search_string = update.message.text.replace("\n", " - ")
+    logger_search.info(
+        f"{log_command}  {log_user_id} {log_user_name} "
+        f"({log_user_full_name}) - {log_search_string}"
+    )
+
+    search_string = update.message.text
+    mes = update.message.reply_text("Подождите, идёт поиск...")
+
+    if "\n" in search_string:
+        title, author = search_string.split("\n", maxsplit=1)
+        libr = flib.scrape_books_mbl(title, author)
+    else:
+        libr = flib.scrape_books(search_string)
+
+    if libr is None:
+        context.bot.deleteMessage(
+            chat_id=mes.chat_id, message_id=mes.message_id)
+        update.message.reply_text("К сожалению, ничего не найдено =(")
+    else:
+        kb = []
+        for i in range(len(libr)):
+            book = libr[i]
+            text = f"{book.title} - {book.author}"
+            callback_data = "find_book_by_id " + book.id
+            kb.append([InlineKeyboardButton(text,
+                                            callback_data=callback_data)])
+
+        reply_markup = InlineKeyboardMarkup(kb)
+
+        update.message.reply_text("Выберите книгу:", reply_markup=reply_markup)
+        context.bot.deleteMessage(
+            chat_id=mes.chat_id, message_id=mes.message_id)
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user
+    # is needed
+    # Some clients may have trouble otherwise.
+    # See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    command, arg = query.data.split(" ", maxsplit=1)
+    if command == "find_book_by_id":
+        find_book_by_id(book_id=arg, update=update, context=context)
+    if command == "get_book_by_format":
+        get_book_by_format(data=arg, update=update, context=context)
+
+
+def find_book_by_id(book_id, update: Update, context: CallbackContext):
+
+    log_command = "find_book_by_id".ljust(20)
+    log_user_id = update.effective_user.id
+    log_user_name = update.effective_user.name
+    log_user_full_name = update.effective_user.full_name
+    log_search_string = book_id
+    logger_search.info(
+        f"{log_command}  {log_user_id} {log_user_name} "
+        f"({log_user_full_name}) - {log_search_string}"
+    )
+
+    mes = context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Подождите, идёт загрузка..."
+    )
+    book = flib.get_book_by_id(book_id)
+    capt = "\U0001F4D6 {title}\n\U0001F5E3 {author}".format(
+        author=book.author, title=book.title
+    )
+
+    kb = []
+    for b_format in book.formats:
+        text = b_format
+        callback_data = f"get_book_by_format {book.id}+{b_format}"
+        kb.append([InlineKeyboardButton(text, callback_data=callback_data)])
+    reply_markup = InlineKeyboardMarkup(kb)
+
+    if book.cover:
+        flib.download_book_cover(book)
+        c_full_path = os.path.join(os.getcwd(), "books", book_id, "cover.jpg")
+        cover = open(os.path.join(c_full_path), "rb")
+        context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=cover,
+            caption=capt,
+            reply_markup=reply_markup,
+        )
+    else:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="[обложки нет]\n\n" + capt,
+            reply_markup=reply_markup,
+        )
+    context.bot.deleteMessage(chat_id=mes.chat_id, message_id=mes.message_id)
+
+
+def get_book_by_format(data: str, update: Update, context: CallbackContext):
+
+    log_command = "get_book_by_format".ljust(20)
+    log_user_id = update.effective_user.id
+    log_user_name = update.effective_user.name
+    log_user_full_name = update.effective_user.full_name
+    logger_search.info(
+        f"{log_command}  {log_user_id} {log_user_name} "
+        f"({log_user_full_name}) - {data}"
+    )
+
+    mes = context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Подождите, идёт скачивание..."
+    )
+
+    book_id, book_format = data.split("+")
+    book = flib.get_book_by_id(book_id)
+
+    b_full_path = flib.download_book(book, book_format)
+
+    if b_full_path:
+        file = open(os.path.join(b_full_path), "rb")
+        context.bot.send_document(
+            chat_id=update.effective_chat.id, document=file)
+        context.bot.deleteMessage(
+            chat_id=mes.chat_id, message_id=mes.message_id)
+    else:
+        logger_search.info(
+            f"{log_command}  {log_user_id} {log_user_name} "
+            f"({log_user_full_name}) - {data} - download error"
+        )
+        context.bot.deleteMessage(
+            chat_id=mes.chat_id, message_id=mes.message_id)
+        mes = context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Произошла ошибка на сервере."
+        )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Displays info on how to use the bot."""
+    update.message.reply_text("Нажмите /start чтобы начать")
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Updater and pass it your bot`s token.
+    TOKEN = settings["TOKEN"]
+    updater = Updater(TOKEN)
+
+    updater.dispatcher.add_handler(CommandHandler("start", start_callback))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler("help", help_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, find_the_book))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
